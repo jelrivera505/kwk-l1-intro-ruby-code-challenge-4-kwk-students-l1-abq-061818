32fe1212967a56eeb89a63ec9223fabd2c67af55
jelrivera505 <jelrivera505@gmail.com>
2018-06-21 18:28:58 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,37 @@
-#write out your code here
-
 def least_coins(cents)
 
-#Code your answer here!
+change={} 
 
-end
+  quarters= cents/25
+  puts quarters
+  quartersR= cents%25
+  puts quartersR
+  
+ dimes = quartersR/10 
+ puts dimes 
+ dimesR= quartersR%10 
+ puts dimesR
+  
+ nickles= dimesR/5 
+ puts nickles
+ nicklesR= dimesR%5 
+ puts nicklesR
+ 
+ pennies= nicklesR/1 
+ puts pennies
+ penniesR= nicklesR%1 
+ puts penniesR 
+
+change[:quarters]= quarters
+puts change
+change[:dimes]= dimes 
+puts change
+change[:nickels] =nickles 
+puts change
+change[:pennies]= pennies 
+puts change
+
+return change
+end 
+
+least_coins(29)
